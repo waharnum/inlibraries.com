e406c0f348eb63fc18a52112d931d74235735634
Alan Harnum <waharnum@gmail.com>
2016-02-18 18:07:40 -0500

    Better synonyms implementation

--- in_libraries.py
+++ in_libraries.py
@@ -3,23 +3,26 @@ from flask import render_template
 from flask import request
 from datetime import date
 from random import randint
+import json
 
 app = Flask(__name__)
 
 # TODO: remove before production
 app.debug = True
 
+# Load synonyms
+synonyms_json = open("synonyms.json").read()
+synonyms = json.loads(synonyms_json)
+
+# part = part of speech (noun, verb, etc)
+# word = specific word to get a synonym for
+def get_synonym(part, word):
+    synonyms_for_word = synonyms[part][word]
+    random_number = randint(0, len(synonyms_for_word)-1)
+    return synonyms_for_word[random_number]
+
 # Set up Jinja tags
-
-
-
-def get_synonym_leadership():
-    synonyms_leadership = ["leadership", "administration", "management"]
-    random_number = randint(0, len(synonyms_leadership)-1)
-    return synonyms_leadership[random_number]
-    return u'leadership'
-
-app.jinja_env.globals.update(synonym_leadership=get_synonym_leadership)
+app.jinja_env.globals.update(synonym=get_synonym)
 
 @app.route('/')
 def front_page(subdomain=None):
