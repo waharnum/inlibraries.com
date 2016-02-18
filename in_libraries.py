ea372aba8b0df5dce32ce774b1ce33a418e7727c
Alan Harnum <waharnum@gmail.com>
2016-02-18 14:12:37 -0500

    basic synonym generation that needs refactoring

--- in_libraries.py
+++ in_libraries.py
@@ -2,11 +2,24 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from datetime import date
+from random import randint
 
 app = Flask(__name__)
 
 # TODO: remove before production
 app.debug = True
+
+# Set up Jinja tags
+
+
+
+def get_synonym_leadership():
+    synonyms_leadership = ["leadership", "administration", "management"]
+    random_number = randint(0, len(synonyms_leadership)-1)
+    return synonyms_leadership[random_number]
+    return u'leadership'
+
+app.jinja_env.globals.update(synonym_leadership=get_synonym_leadership)
 
 @app.route('/')
 def front_page(subdomain=None):
