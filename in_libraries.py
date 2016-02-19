2dbdd16cc22ed2dc1982d40bc77e66b4081f30f8
Alan Harnum <waharnum@gmail.com>
2016-02-19 10:39:49 -0500

    Basic madlibs-type implementation for session templates

--- in_libraries.py
+++ in_libraries.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import render_template_string
 from flask import request
 from datetime import date
 from random import randint
@@ -13,6 +14,10 @@ app.debug = True
 # Load synonyms
 synonyms_json = open("synonyms.json").read()
 synonyms = json.loads(synonyms_json)
+
+# Load session madlibs
+session_madlibs_json = open("session_madlibs.json").read()
+session_madlibs = json.loads(session_madlibs_json)
 
 # part = part of speech (noun, verb, etc)
 # word = specific word to get a synonym for
@@ -32,6 +37,15 @@ def front_page(subdomain=None):
     subdomain = " ".join(subdomain_split)
     subdomain_title = subdomain.title()
     year = date.today().year
+
+    def get_random_session_madlib():
+        random_number = randint(0, len(session_madlibs)-1)
+        template_string = session_madlibs[random_number]
+        return render_template_string(template_string, subdomain=subdomain, subdomain_title=subdomain_title, year=year)
+
+    # Set up Jinja tag for random session madlib in templates
+    app.jinja_env.globals.update(random_session=get_random_session_madlib)
+    
     return render_template('front_page.html', subdomain=subdomain, subdomain_title=subdomain_title, year=year)
 
 if __name__ == '__main__':
