6f2cdc62fb2c0ceed2f179f12e2e672220635e60
Alan Harnum <waharnum@gmail.com>
2016-02-19 12:14:51 -0500

    refactoring towards easier testing

--- in_libraries.py
+++ in_libraries.py
@@ -40,13 +40,16 @@ def front_page(subdomain=None):
 
     def get_random_session_madlib():
         random_number = randint(0, len(session_madlibs)-1)
-        template_string = session_madlibs[random_number]
+        return get_session_madlib(random_number)
+
+    def get_session_madlib(idx):
+        template_string = session_madlibs[idx]
         return render_template_string(template_string, subdomain=subdomain, subdomain_title=subdomain_title, year=year)
 
     # Set up Jinja tag for random session madlib in templates
     app.jinja_env.globals.update(random_session=get_random_session_madlib)
-    
-    return render_template('front_page.html', subdomain=subdomain, subdomain_title=subdomain_title, year=year)
+
+    return render_template('front_page.html', subdomain=subdomain, subdomain_title=subdomain_title, year=year, test_session_madlibs=False)
 
 if __name__ == '__main__':
     app.run()
